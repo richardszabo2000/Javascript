3798f9c12c28af2129be2334803c59838242eebb
richardszabo2000 <66848025+richardszabo2000@users.noreply.github.com>
2020-06-13 12:11:52 +0200

    Create 01-My_first_exercise.js

---
+++ Exercise/01-My_first_exercise.js
@@ -0,0 +1,14 @@
+/* Hey, this is my first script c:
+ Current version: v1.0
+ */
+var name  = prompt("Set a name [Options: Bob, David / other]");
+if (name === "Bob") {
+    alert("Hey, Bob.");
+    end;
+}
+if (name === "David") {
+    alert("Hi, David.");
+}
+else {
+    alert("I don't know you. :c");
+}
